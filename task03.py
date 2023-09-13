c88f254a14d53f49d020aa98b5387e5c7ac3d55a
vprivalova <privalovaa.vv@gmail.com>
2023-09-13 09:59:40 +0700

    commit task 2,3

--- task03.py
+++ task03.py
@@ -1,4 +1,16 @@
-'''
+"""
 Задание 3.
 Дан текст. Определить, сколько различных букв в нем.
-'''
+"""
+
+
+text = input()
+count = 0
+letters = ''
+for item in text:
+    if item.isalpha():
+        if item not in letters:
+            letters = letters + item
+list_of_letters = sorted(letters)
+print(len(list_of_letters))
+
