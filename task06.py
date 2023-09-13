480ece446c9131471b53ec3ba7420e4e98726794
vprivalova <privalovaa.vv@gmail.com>
2023-09-13 12:02:49 +0700

    commit task 6

--- task06.py
+++ task06.py
@@ -1,4 +1,18 @@
-'''
+"""
 Задание 6.
 Дано предложение. Напечатать его в обратном порядке слов.
-'''
+"""
+
+
+sentence = input().split()
+
+punct_sign = str(sentence[-1])[-1]
+reverse_sentence = sentence[::-1]
+reverse_sentence.pop(0)
+reverse_sentence.insert(0, str(sentence[-1])[:-1])
+result = ' '.join(reverse_sentence)
+
+if result[-1] != ',':
+    print(result.capitalize() + punct_sign)
+else:
+    print(result[:-1].capitalize() + punct_sign)
