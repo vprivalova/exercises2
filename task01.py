d0f270e3791a5d04a5821d79580adb16075fa893
vprivalova <privalovaa.vv@gmail.com>
2023-09-13 09:36:17 +0700

    commit task 1

--- task01.py
+++ task01.py
@@ -4,6 +4,17 @@
 """
 
 
+ptr = input()
+count = 0
+count_values = []
+for item in ptr:
+    if item.isspace():
+        count = count + 1
+    else:
+        count_values.append(count)
+        count = 0
+print(max(count_values))
+
 
 
 
