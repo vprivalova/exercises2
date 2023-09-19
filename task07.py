b9d2994fcd0004415e3371a87e3ea4a510e30ff6
vprivalova <privalovaa.vv@gmail.com>
2023-09-19 09:27:10 +0700

    commit task 7

--- task07.py
+++ task07.py
@@ -1,4 +1,20 @@
-'''
+"""
 Задание 7.
 Дано предложение. Найти длину его самого короткого слова.
-'''
+"""
+
+signs = '.,?!():"'
+min_len = 100
+
+sentence = input().split()
+
+for word in sentence:
+    for letter in word:
+        if letter in signs:
+            word = word.replace(letter, '')
+
+for item in sentence:
+    if len(item) < min_len:
+        min_len = len(item)
+
+print(min_len)
