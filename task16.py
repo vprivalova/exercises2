074d2d0ee317847317261e189307840af45c56b6
vprivalova <privalovaa.vv@gmail.com>
2023-09-24 00:56:57 +0700

    commit task 16

--- task16.py
+++ task16.py
@@ -5,3 +5,23 @@
 скобка, а слева от каждой закрывающей — соответствующаяей закрывающая).
 """
 
+text = input()
+text = list(text)
+symbols = list(filter(lambda x: x in '()', text))
+error = 0
+
+if len(symbols) % 2 != 0:
+    print('Круглые скобки расставлены в тексте не корректно')
+else:
+    for index in range(len(symbols)):
+        if index % 2 == 0:
+            if symbols[index] != '(':
+                error = error + 1
+        else:
+            if symbols[index] != ')':
+                error = error + 1
+
+    if error == 0:
+        print('Круглые скобки расставлены в тексте корректно.')
+    else:
+        print('Круглые скобки расставлены в тексте не корректно.')
