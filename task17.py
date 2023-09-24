9c2826b032f985e06d7ece0674c4ebca398db95f
vprivalova <privalovaa.vv@gmail.com>
2023-09-25 03:28:31 +0700

    intermediate commit

--- task17.py
+++ task17.py
@@ -5,4 +5,21 @@
 Функцию eval() не использовать.
 """
 
+task = input()
+task = list(task)
+symbols = list(filter(lambda x: x in '()+-*/1234567890', task))
+print(symbols)
+max_ind_lft = 0
+max_ind_rght = 0
+result = 0
+cnt = symbols.count('(')
+
+for i in range(len(symbols)):
+    if symbols[i] == '(':
+        max_ind_lft = i
+for j in range(max_ind_lft, 100000):
+    if symbols[j] == ')':
+        max_ind_rght = j
+
+
 
